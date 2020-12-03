8dde830279147d269195f7b573a4ff6455278504
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-03 06:40:40 -0500

    Day 3, Part 2

--- day03.js
+++ day03.js
@@ -1,21 +1,18 @@
 'use strict'
-//6:21 AM
-//6:29 AM incorrect answer: 8
+//6:21 AM part 1 begin
+//6:29 AM incorrect... FORGOT TO MODULO!!!
 //6:31 AM correct answer 216
 
+//6:34 AM part 2 begin
+//6:39 AM part 2 correct answer
 const TREE = '#';
-const rowIncrememnt = 1;
-const colIncrement = 3;
-let rowIndex = 0;
-let colIndex = 0;
-let treeCount = 0;
 
-// Part 1
-// ======
-
-const part1 = input => {
+const getTrees = (input, rowIncrememnt, colIncrement) => {
+  let rowIndex = 0;
+  let colIndex = 0;
+  let treeCount = 0;
   const data = input.split('\n');
-  do {
+  while (rowIndex + rowIncrememnt < data.length) {
     rowIndex += rowIncrememnt;
     colIndex += colIncrement;
 
@@ -23,16 +20,23 @@ const part1 = input => {
     if (row.charAt(colIndex % row.length) === TREE) {
       treeCount++
     }
-  } while (rowIndex < data.length - 1)
+  } 
 
   return treeCount;
+}
+
+// Part 1
+// ======
+
+const part1 = input => {
+  return getTrees(input, 1, 3);
 }
 
 // Part 2
 // ======
 
 const part2 = input => {
-  return input
+  return getTrees(input, 1, 1) * getTrees(input, 1, 3) * getTrees(input, 1, 5) * getTrees(input, 1, 7) * getTrees(input, 2, 1);
 }
 
 module.exports = { part1, part2 }
