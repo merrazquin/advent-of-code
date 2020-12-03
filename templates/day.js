11399069abe9fff33e6ffe9952d24d079686e969
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-03 08:32:33 -0500

    Template customization

---
+++ templates/day.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Setup
+
+// Part 1
+// ======
+
+const part1 = input => {
+  return input
+}
+
+// Part 2
+// ======
+
+const part2 = input => {
+  return input
+}
+
+module.exports = { part1, part2 }
