50709b69d245f78415e9a6f521517e5c5da8b824
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-05 01:15:20 -0500

    Day 5, cleanup

--- day05/script.js
+++ day05/script.js
@@ -1,11 +1,9 @@
 'use strict'
 // Setup
+
 const getRange = (min, max, lower) => {
-  if (max - min === 1) {
-    return [lower ? min : max, -1]
-  }
   const mid = min + Math.floor((max - min - 1) / 2)
-  
+
   if (lower) {
     return [min, mid]
   } else {
@@ -13,12 +11,8 @@ const getRange = (min, max, lower) => {
   }
 }
 
-// Part 1
-// ======
-
-const part1 = input => {
-
-  return input.split('\n')
+const getSeatIDs = boardingPasses => {
+  return boardingPasses.split('\n')
     .map(boardingPass => {
       let rowRangeMin = 0
       let rowRangeMax = 127
@@ -35,8 +29,15 @@ const part1 = input => {
       })
 
       return 8 * rowRangeMin + colRangeMin
-    
     })
+}
+
+// Part 1
+// ======
+
+const part1 = input => {
+
+  return getSeatIDs(input)
     .sort((a, b) => b - a)
     .shift()
 
@@ -46,30 +47,14 @@ const part1 = input => {
 // ======
 
 const part2 = input => {
-  const seats = input.split('\n')
-    .map(boardingPass => {
-      let rowRangeMin = 0
-      let rowRangeMax = 127
-      let colRangeMin = 0
-      let colRangeMax = 7
-
-      boardingPass.split('').forEach((instruction, index) => {
-        if (index < 7) {
-          [rowRangeMin, rowRangeMax] = getRange(rowRangeMin, rowRangeMax, instruction === 'F')
-        }
-        else {
-          [colRangeMin, colRangeMax] = getRange(colRangeMin, colRangeMax, instruction === 'L')
-        }
-      })
-
-      return 8 * rowRangeMin + colRangeMin
-    
-    })
+  const seats = getSeatIDs(input)
     .sort((a, b) => a - b)
 
-    for(var i = 68; i < 965; i++) {
-      if (seats.indexOf(i) == -1) return i;
-    }
+  const minSeat = seats[0]
+  const maxSeat = seats[seats.length - 1]
+  for (var seat = minSeat; seat < maxSeat; seat++) {
+    if (!seats.includes(seat)) return seat
+  }
 }
 
 module.exports = { part1, part2 }
