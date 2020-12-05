4629ffc05f19f200369464fe5a08ea42fe6d1953
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-05 02:59:01 -0500

    Day 2, unit tests

---
+++ day02/test.js
@@ -0,0 +1,22 @@
+const assert = require('assert')
+const { part1, part2 } = require('../day02/script')
+
+const passwords = 
+`1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`
+
+describe('Day 2: Password Philosophy', () => {
+    describe('Part One', () => {
+        it('should provide the total number of valid passwords', () => {
+            assert.strictEqual(part1(passwords), 2)
+        })
+    })
+
+    describe ('Part Two', () => {
+        it('should provide the total number of valid passwords (new policy)', () => {
+            assert.strictEqual(part2(passwords), 1)
+        })
+    })
+})
+
