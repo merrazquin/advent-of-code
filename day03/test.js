0da909adc3c67baed96c1ee76262cdaddf2357ad
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-05 03:58:58 -0500

    Day 3, unit test

---
+++ day03/test.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+const { part1, part2 } = require('../day03/script')
+
+const geology = 
+`..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`
+
+describe('Day 3: Toboggan Trajectory', () => {
+    describe('Part One', () => {
+        it('should return the number of trees encountered on a single slope', () => {
+            assert.strictEqual(part1(geology), 7)
+        })
+    })
+
+    describe ('Part Two', () => {
+        it('should find the product of trees across multiple slopes', () => {
+            assert.strictEqual(part2(geology), 336)
+        })
+    })
+})
