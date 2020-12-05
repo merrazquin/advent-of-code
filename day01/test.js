327523faea34f7cf2ad3918c29e4359ff0fdbb83
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-04 23:52:14 -0500

    Start adding unit tests

---
+++ day01/test.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+const { part1, part2 } = require('../day01/script')
+const numbers =
+    `1721
+    979
+    366
+    299
+    675
+    1456`
+
+describe('Day 1: Report Repair', () => {
+    describe('Part One', () => {
+        it('should return the product of 2 numbers which sum to 2020', () => {
+            assert.strictEqual(part1(numbers), 514579)
+        })
+    })
+
+    describe ('Part Two', () => {
+        it('should return the product of 3 numbers which sum to 2020', () => {
+            assert.strictEqual(part2(numbers), 241861950)
+        })
+    })
+})
+
