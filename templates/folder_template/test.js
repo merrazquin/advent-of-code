27a6b580ce0bab6046e8e89a2a4605a37bdc83ec
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-11 16:28:52 -0500

    Folder structure template

---
+++ templates/folder_template/test.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const { part1, part2 } = require('./script')
+
+const [testInputVariable] = 
+`[testInput]
+...`
+
+describe('Day [dayNumber]: [dayDescription]', () => {
+    describe('Part One', () => {
+        it('[assertionDescription]', () => {
+            assert.strictEqual(part1([testInput]), [assertionValue])
+        })
+    })
+
+    describe ('Part Two', () => {
+        it('', () => {
+            assert.strictEqual(part2([testInput]), [assertionValue])
+        })
+    })
+})
