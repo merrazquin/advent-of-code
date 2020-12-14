310ddd6132909366d44389501829c85a3ee81079
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-14 08:02:06 -0500

    Year 2020, Day 14, Part 2 Start

--- year-2020/day14/test.js
+++ year-2020/day14/test.js
@@ -7,6 +7,12 @@ mem[8] = 11
 mem[7] = 101
 mem[8] = 0`
 
+const initProgram2 =
+`mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`
+
 describe('Day 14: Docking Data', () => {
     describe('Part One', () => {
         it('should return the sum of all values left in memory after program completes', () => {
@@ -14,9 +20,9 @@ describe('Day 14: Docking Data', () => {
         })
     })
 
-    describe.skip('Part Two', () => {
-        it('', () => {
-            assert.strictEqual(part2(initProgram), 165)
+    describe('Part Two', () => {
+        it('should return the sum of all values left in memory after the program completes', () => {
+            assert.strictEqual(part2(initProgram2), 208)
         })
     })
 })
