d7dba83f10066add545cc74fd7cb55457851d440
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-14 07:17:54 -0500

    Year 2020, Day 14 Start

---
+++ year-2020/day14/test.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+const { part1, part2 } = require('./script')
+
+const initProgram = 
+``
+
+describe('Day 14: Docking Data', () => {
+    describe('Part One', () => {
+        it('should return the sum of all values left in memory after program completes', () => {
+            assert.strictEqual(part1(initProgram), 165)
+        })
+    })
+
+    describe ('Part Two', () => {
+        it('', () => {
+            assert.strictEqual(part2(initProgram), 165)
+        })
+    })
+})
