328a5ff2a7c8ec282c7cba0279ae1c170d3717b9
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-16 12:53:07 -0500

    Year 2020, Day 16, Second Star 16   08:06:13  15787      0   09:00:33  11566      0

--- year-2020/day16/script.js
+++ year-2020/day16/script.js
@@ -33,9 +33,16 @@ const prodRanges = () => {
 const testRanges = () => {
     const ranges = []
     ranges.push({field: 'class', range1: {min:1, max:3}, range2:{min:5, max:7}})
-ranges.push({field: 'row', range1: {min:6, max:11}, range2:{min:33, max:44}})
-ranges.push({field: 'seat', range1: {min:13, max:40}, range2:{min:45, max:50}})
+    ranges.push({field: 'row', range1: {min:6, max:11}, range2:{min:33, max:44}})
+    ranges.push({field: 'seat', range1: {min:13, max:40}, range2:{min:45, max:50}})
     return ranges
+}
+const testRanges2 = () => {
+    const ranges = []
+    ranges.push({field: 'class', range1: {min:0, max:1}, range2:{min:4, max:19}})
+    ranges.push({field: 'row', range1: {min:0, max:5}, range2:{min:8, max:19}})
+    ranges.push({field: 'seat', range1: {min:0, max:13}, range2:{min:16, max:19}})
+    return ranges    
 }
 const testTickets = () => {
     const tix = 
@@ -48,7 +55,9 @@ return tix.split('\n').map(row => {
 }).join(',').split(',').map(num => parseInt(num))
 }
 const prodTickets = () => {
-    return '<REDACTED>'
+}
+const prodTickets2 = () => {
+    return ['<redacted>']
 }
 
 const part1 = (input, debug = false) => {
@@ -63,9 +72,67 @@ const part1 = (input, debug = false) => {
 
 // Part 2
 // ======
+const isViablePosition = (positionValues, field) => {
+    return positionValues.every(value => isValueValid(value, [field]))
+    
+}
+const removePositionFromAllExcept = (position, mapping, except) => {
+    const newMapping = {}
+    for (let fieldName in mapping) {
+        let positions = mapping[fieldName];
+        if (fieldName != except) {
+            const index = positions.indexOf(position)
+            if (index != -1) {
+                positions.splice(index, 1)
+            }
+        }
+        newMapping[fieldName] = positions
+    }
+    return newMapping
+}
+const part2 = (input, debug = false) => {
+    const ranges = debug ? testRanges2() : prodRanges()
+    const tickets = (debug ? testTickets2() : prodTickets2()).filter(ticket => isTicketValid(ticket, ranges))
+    const numFields = ranges.length
+    let mapping = {}
+    ranges.forEach(range => {
+        mapping[range.field] = []
+        for (let fieldNum = 0; fieldNum < numFields; fieldNum++) {
+            const column = tickets.map(ticket => ticket[fieldNum])
+            if (isViablePosition(column, range)) {
+                mapping[range.field].push(fieldNum)
+            }
+        }
+    })
 
-const part2 = input => {
-    return preprocessing(input)
+    while (true) {
+        let breakout = true
+        for (let fieldName in mapping) {
+            const positions = mapping[fieldName];
+            if (positions.length == 1) {
+                // strip position from all but fieldName
+                mapping = removePositionFromAllExcept(positions[0], mapping, fieldName)
+            } else {
+                breakout = false
+            }
+        }
+        if (breakout) {
+            break
+        }
+    }
+    
+    const myTicket = '<redacted>'
+    let product = 1
+    for (let fieldName in mapping) {
+        if (mapping.hasOwnProperty(fieldName)) {
+            const position = mapping[fieldName];
+            if (fieldName.includes('departure')) {
+                console.log('adding', fieldName, '(',position,')', 'with value of', myTicket[position])
+                product *= myTicket[position]
+            }
+        }
+    }
+    return product
 }
 
 module.exports = { part1, part2 }
