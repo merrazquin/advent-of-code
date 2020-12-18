725d4c892c0d9731ec0579ed8b513907ce932d68
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-18 08:21:16 -0500

    Year 2020, Day 18, Part 1 done

--- year-2020/day18/script.js
+++ year-2020/day18/script.js
@@ -1,16 +1,56 @@
 'use strict'
 
 // Setup
-
+const {sumAll} = require('../../utils')
 const preprocessing = input => {
     return input.split('\n')
+}
+
+const evaluateExpressionLeftToRight = expression => {
+    const exprRegExp = /(\d+ .{1} \d+)/
+    let match = exprRegExp.exec(expression)
+    while (match) {
+        expression = expression.split('')
+        expression.splice(match.index, match[0].length, eval(match[0]))
+        expression = expression.join('')
+        if(/\(\d+\)/.exec(expression)) {
+            expression = eval(expression)
+        }
+
+        match = exprRegExp.exec(expression)
+    }
+    return eval(expression)
+}
+
+const evaluateParentheticals = expression => {
+    const parenRegExp = /\([^\(\)]+\)/
+    let match = parenRegExp.exec(expression)
+    while(match) {
+        expression = expression.split('')
+        expression.splice(match.index, match[0].length, evaluateExpressionLeftToRight(match[0]))
+        expression = expression.join('')
+        match = parenRegExp.exec(expression)
+    }
+    return evaluateExpressionLeftToRight(expression)
+}
+
+const evaluateSimpleParentheticals = expression => {
+    const parenRegExp = /\(\d+ .{1} \d+\)/
+    let match = parenRegExp.exec(expression)
+    while (match) {
+        expression = expression.split('')
+        expression.splice(match.index, match[0].length, eval(match[0]))
+        expression = expression.join('')
+        match = parenRegExp.exec(expression)
+    }
+    return evaluateExpressionLeftToRight(expression)
 }
 
 // Part 1
 // ======
 
 const part1 = input => {
-    return preprocessing(input)
+    return sumAll(preprocessing(input).map(expression => evaluateParentheticals(expression)));
 }
 
 // Part 2
@@ -20,4 +60,4 @@ const part2 = input => {
     return preprocessing(input)
 }
 
-module.exports = { part1, part2 }
+module.exports = { part1, part2, evaluateExpression: evaluateSimpleParentheticals, evaluateExpressionLeftToRight, evaluateParentheticals }
--- year-2020/day18/test.js
+++ year-2020/day18/test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const { part1, part2 } = require('./script')
+const { part1, part2, evaluateExpression, evaluateExpressionLeftToRight, evaluateParentheticals } = require('./script')
 
 const homework = 
 `1 + 2 * 3 + 4 * 5 + 6
@@ -8,8 +8,18 @@ const homework =
 5 + (8 * 3 + 9 + 3 * 4 * 3)
 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
 ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`
+/*
+(54 * 126 + 6) + 2 + 4 * 2
 
+*/
 describe('Day 18: Operation Order', () => {
+    describe('evaluateExpressionLeftToRight', () => {
+        it('should evaluate an expression from left to right', () => {
+            assert.strictEqual(evaluateParentheticals('1 + 2 * 3 + 4 * 5 + 6'), 71)
+            assert.strictEqual(evaluateParentheticals('5 + (8 * 3 + 9 + 3 * 4 * 3)'), 437)
+            assert.strictEqual(evaluateParentheticals('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'), 13632)
+        })
+    })
     describe('Part One', () => {
         it('should evaluate the expression on each line of homework and provide the sum of the resulting values', () => {
             assert.strictEqual(part1(homework), 26457)
