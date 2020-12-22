c22dc6963fdf999685de9933d5ef36d6175e3948
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-22 07:17:23 -0500

    Year 2020, Day 22, Gameplay complete

--- year-2020/day22/script.js
+++ year-2020/day22/script.js
@@ -2,8 +2,8 @@
 
 // Setup
 const playRound = (deck1, deck2) => {
-    card1 = deck1.shift()
-    card2 = deck2.shift()
+    let card1 = deck1.shift()
+    let card2 = deck2.shift()
 
     if (card1 > card2) {
         deck1.push(card1, card2)
@@ -12,14 +12,31 @@ const playRound = (deck1, deck2) => {
     }
 }
 const preprocessing = input => {
-    return input.split('\n')
+    let [deck1, deck2] = input.split('\n\n')
+    deck1 = deck1.split('\n').slice(1).map(card => parseInt(card))
+    deck2 = deck2.split('\n').slice(1).map(card => parseInt(card))
+    return {
+        deck1, 
+        deck2
+    }
 }
 
 // Part 1
 // ======
+const calculateWinningScore = winningDeck => {
+
+}
 
 const part1 = input => {
-    return preprocessing(input)
+    const {deck1, deck2} = preprocessing(input)
+
+    let round = 0
+    while (deck1.length && deck2.length) {
+        round++
+        playRound(deck1, deck2)
+    }
+
+    console.log('game won after', round, 'rounds')
 }
 
 // Part 2
