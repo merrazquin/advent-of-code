5a3dbb65f1f80c9ba024bedf3080667132c786d4
Marcela Errazquin <marcela.errazquin@disney.com>
2020-12-23 08:38:39 -0500

    Year 2020, Day 23, Part 1 unit tests passing

--- year-2020/day23/script.js
+++ year-2020/day23/script.js
@@ -44,10 +44,10 @@ const preprocessing = input => {
     let currentCup = cupList.head
     currentCup.prev = cupList.tail
     cupList.tail.next = currentCup
-
     return {
         currentCup,
-        cupList
+        cupList,
+        cupNodes
     }
 }
 
@@ -56,18 +56,49 @@ const preprocessing = input => {
 // ======
 
 const part1 = (input, moves) => {
-    const {currentCup, cups} = preprocessing(input)
+    let {currentCup, cupList, cupNodes} = preprocessing(input)
 
-    // maintain an array of nodes to easily access them
-    // create a circular linked list of cups
-    // record the "head/current" (first cup listed)
-    // on a move, grab the 3 cups to the right of the "head"
-    // detatch those 3 cups from the head, grab cup attached to "tail" of 3 cups, and attach it to the "head"
-    // find a destination cup (cup equal to "head" - 1, wrapping around to highest if necessary)
-    // position 3 cups in between destination and it's "next"
-    // select new "head" by grabbing the cup directly to right of the current "head"
+    // maintain an array of nodes to easily access them (cupNodes)
+    // create a circular linked list of cups (cups)
+    // record the "head/current" (first cup listed) (currentCup)
+    while (moves) {
+        // on a move, grab the 3 cups to the right of the "head"
+        // detatch those 3 cups from the head, grab cup attached to "tail" of 3 cups, and attach it to the "head"
+        let next3Cups = new Cups(currentCup.next, currentCup.next.next, currentCup.next.next.next)
+        // find a destination cup (cup equal to "head" - 1, wrapping around to highest if necessary)
+        const currCupLabel = parseInt(currentCup.toString())
+        let destinationLabel = currCupLabel - 1
+        if (destinationLabel < 1) {
+            destinationLabel = cupNodes.length - 1
+        }
+        let destinationCup = cupNodes[destinationLabel]
+        while (destinationCup.list != cupList) { // this could be a p2 gotcha
+            destinationLabel--
+            if (destinationLabel < 1) {
+                destinationLabel = cupNodes.length - 1
+            }
+            destinationCup = cupNodes[destinationLabel]
+        }
+        // position 3 cups in between destination and it's "next"
+        destinationCup.append(next3Cups.head)
+        destinationCup.next.append(next3Cups.head)
+        destinationCup.next.next.append(next3Cups.head)
+        // select new "head" by grabbing the cup directly to right of the current "head"
+        currentCup = currentCup.next
+        
+        moves--
+    }
  
     // after n moves, return a string of all cups in order except `1`
+    let startingNode = cupNodes[1]
+    let nextNode = startingNode.next
+    const finalString = [nextNode.toString()]
+    do {
+        nextNode = nextNode.next
+        finalString.push(nextNode.toString())
+    } while(nextNode.next != startingNode)
+
+    return finalString.join('')
 }
 
 // Part 2
--- year-2020/day23/test.js
+++ year-2020/day23/test.js
@@ -6,14 +6,14 @@ const cups = '389125467'
 describe('Day 23: Crab Cups', () => {
     describe('Part One', () => {
         it('should return the labels on the cups (other than cup 1) after n moves', () => {
-            assert.strictEqual(part1(cups, 10), 67384529)
-            assert.strictEqual(part1(cups, 100), 67384529)
+            assert.strictEqual(part1(cups, 10), '92658374')
+            assert.strictEqual(part1(cups, 100), '67384529')
         })
     })
 
     describe.skip('Part Two', () => {
         it('', () => {
-            assert.strictEqual(part2(cups), 67384529)
+            assert.strictEqual(part2(cups), '67384529')
         })
     })
 })
