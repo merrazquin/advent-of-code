68ed65e579cd374a3020f53a8a180347ea04fcc2
Marcela Errazquin <marcela.errazquin@disney.com>
2021-12-10 08:41:34 -0500

    Year 2021, Day 10 - Cleanup

--- year-2021/day10/script.js
+++ year-2021/day10/script.js
@@ -27,7 +27,7 @@ const revLookup = {
     '<': 4
 }
 
-const findIllegalCharacter = line => {
+const processLine = (line, breakOnError = true) => {
     const currentlyOpen = []
     const openChars = ['(', '[', '{', '<']
     const chars = line.trim().split('')
@@ -42,29 +42,11 @@ const findIllegalCharacter = line => {
             currentlyOpen.pop()
             continue
         }
-        return lookup[char].errorScore
+        return breakOnError ? lookup[char].errorScore : 0
     }
-    return 0
+    return breakOnError ? 0 : findCompletionScore(currentlyOpen)
 }
 
-const findCurrentlyOpen = line => {
-    const currentlyOpen = []
-    const openChars = ['(', '[', '{', '<']
-    const chars = line.trim().split('')
-    for (let index = 0; index < chars.length; index++) {
-        const char = chars[index]
-        const isOpeningChar = openChars.includes(char)
-        if (isOpeningChar) {
-            currentlyOpen.push(char)
-            continue
-        }
-        if (lookup[char].open == currentlyOpen[currentlyOpen.length - 1]) {
-            currentlyOpen.pop()
-            continue
-        }
-    }
-    return currentlyOpen
-}
 const findCompletionScore = chars => {
     return chars.reverse().reduce((score, char) => score * 5 + revLookup[char], 0)
 }
@@ -73,7 +55,7 @@ const findCompletionScore = chars => {
 // ======
 
 const part1 = input => {
-    return sumAll(input.trim().split('\n').map(line => findIllegalCharacter(line)))
+    return sumAll(input.trim().split('\n').map(line => processLine(line)))
 }
 
 // Part 2
@@ -81,10 +63,11 @@ const part1 = input => {
 
 const part2 = input => {
     const completionScores = input.trim().split('\n')
-        .filter(line => findIllegalCharacter(line) === 0)
-        .map(line => findCompletionScore(findCurrentlyOpen(line)))
+        .map(line => processLine(line, false))
+        .filter(score => score > 0)
+        .sort((a, b) => a-b)
 
-    return completionScores.sort((a, b) => a-b)[Math.floor(completionScores.length / 2)]
+    return completionScores[Math.floor(completionScores.length / 2)]
 }
 
 module.exports = { part1, part2 }
