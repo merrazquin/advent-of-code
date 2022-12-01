3e67fcd57e2314b445c3d6663eaae89bd6ce6a6b
Marcela <marcela.errazquin@disney.com>
2022-12-01 01:27:17 -0500

    aoc runner setup

---
+++ year-2022/src/lib.rs
@@ -0,0 +1,10 @@
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+
+// If you bring any dependencies, extern crate them here
+//extern crate itertools;
+
+pub mod day1;
+
+aoc_lib!{ year = 2022 }
---
+++ year-2022/src/main.rs
@@ -0,0 +1,9 @@
+#![feature(array_windows)]
+
+extern crate aoc_2022;
+extern crate aoc_runner_derive;
+extern crate aoc_runner;
+
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = aoc_2022 }
