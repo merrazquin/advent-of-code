7935e563c64b9693e2d835f642b6df583e87576f
Marcela <marcela.errazquin@disney.com>
2023-12-04 07:33:17 -0500

    Update template to include preProcessing Add test template and new scripts for initializing and testing a single day

--- templates/day.js
+++ templates/day.js
@@ -1,19 +1,20 @@
 'use strict'
 
 // Setup
+const preProcessing = input => input
 
 // Part 1
 // ======
 
 const part1 = input => {
-    return input
+    return preProcessing(input)
 }
 
 // Part 2
 // ======
 
 const part2 = input => {
-    return input
+    return preProcessing(input)
 }
 
 module.exports = { part1, part2 }
