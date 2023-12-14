01249033d397ba776221e1ffec0fa4bd1837b3ef
Marcela Errazquin <merrazquin@gmail.com>
2023-12-14 17:34:13 -0500

    2023.12.1 optimized

--- year-2023/day12/script.js
+++ year-2023/day12/script.js
@@ -1,41 +1,55 @@
 'use strict'
 
-const { getAllTokenizedPermutations, memoize } = require('../../utils')
+const { sumAll } = require('../../utils')
 
 // Setup
 const preProcessing = input => input.split('\n').map(row => {
     const [springs, sizes] = row.split(' ')
-    return {springs, sizes: sizes.split(',').map(size => parseInt(size))}
+    return {springs: springs.split(''), sizes: sizes.split(',').map(size => parseInt(size))}
 })
 
 // Part 1
 // ======
+const findPermutations = (spring, sizes) => {
+    if (!spring.length) {
+        return (!sizes.length ? 1 : 0)
+    }
 
-// Brute force takes 7 minute for p1... do not recommend 
-const part1Brute = input => {
-    const instructions = preProcessing(input)
-    let total = 0
-    instructions.forEach(instruction => {
-        const memoizedIsViable = wrappedIsViable(instruction.sizes.join('_'))
-        total += getAllTokenizedPermutations(
-            instruction.springs, '?', ['.', '#']
-        ).filter(perm => memoizedIsViable(perm)).length
-    })
-    return total
-}
-const wrappedIsViable = (sizes) => {
-    const memoizedIsViable = memoize(isViable)
-    return function(perm) {
-        const arg = [perm, sizes]
-        return memoizedIsViable.apply(this, arg)
-    } 
-}
-const isViable = (permutation, sizes) => {
-    return permutation.replace(/\.+/g, ' ').trim().split(/\s+/).map(spring => spring.length).join('_') === sizes
+    const firstChar = spring[0]
+    const currSize = sizes[0]
+
+    if (firstChar === '.') {
+        return findPermutations(spring.slice(1), sizes)
+    }
+
+    if (firstChar === '#') {
+        if (spring.length >= currSize
+                && !spring.slice(0, currSize).includes('.')
+                && spring[currSize] !== '#'
+        ) {
+            const newSpring = spring.slice(currSize)
+            if (newSpring[0] === '?') {
+                newSpring[0] = '.'
+            }
+            return findPermutations(newSpring, sizes.slice(1))
+        } else {
+            return 0
+        }
+    }
+
+    if (firstChar === '?') {
+        const springA = spring.slice()
+        springA[0] = '.'
+        const springB = spring.slice()
+        springB[0] = '#'
+
+        return findPermutations(springA, sizes) + findPermutations(springB, sizes)
+    }
 }
 
 const part1 = input => {
-    return part1Brute(input)
+    const instructions = preProcessing(input)
+    return sumAll(instructions.map(instrucion => findPermutations(instrucion.springs, instrucion.sizes)))
 }
 
 // Part 2
@@ -45,4 +59,4 @@ const part2 = input => {
     return 0 // preProcessing(input)
 }
 
-module.exports = { part1, part2 }
+module.exports = { part1, part2, findPermutations }
--- year-2023/day12/test.js
+++ year-2023/day12/test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const { part1, part2 } = require('./script')
+const { part1, part2, findPermutations } = require('./script')
 
 const data = 
 `???.### 1,1,3
@@ -10,6 +10,16 @@ const data =
 ?###???????? 3,2,1`
 
 describe('Day 12:', () => {
+    describe('findPermutations', () => {
+        it('Should find all viable permutations of valid spring arrangements for a single spring/size config', () => {
+            assert.strictEqual(findPermutations('???.###'.split(''), [1,1,3]), 1)
+            assert.strictEqual(findPermutations('.??..??...?##.'.split(''), [1,1,3]), 4)
+            assert.strictEqual(findPermutations('?#?#?#?#?#?#?#?'.split(''), [1,3,1,6]), 1)
+            assert.strictEqual(findPermutations('????.#...#...'.split(''), [4,1,1]), 1)
+            assert.strictEqual(findPermutations('????.######..#####.'.split(''), [1,6,5]), 4)
+            assert.strictEqual(findPermutations('?###????????'.split(''), [3,2,1]), 10)
+        })
+    })
     describe('Part One', () => {
         it('Should find the sum of all possible valid spring arrangements', () => {
             assert.strictEqual(part1(data), 21)
