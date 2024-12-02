609b6497e36228c4e58f7040c64b9eb5bfc272f9
Marcela Errazquin <merrazquin@gmail.com>
2024-12-01 23:53:47 -0500

    2024.01

--- year-2024/day01/day01.js
+++ year-2024/day01/day01.js
@@ -1,45 +1,45 @@
-const axios = require('axios');
-const sessionID = '<REDACTED>';
+const axios = require('axios')
+const sessionID = '<REDACTED>'
 
-const url = 'https://adventofcode.com/2024/day/1/input';
-const cookie = `session=${sessionID}`;
+const url = 'https://adventofcode.com/2024/day/1/input'
+const cookie = `session=${sessionID}`
 
 const demo = `3   4
 4   3
 2   5
 1   3
 3   9
-3   3`;
+3   3`
 
-p1(demo);
+p1(demo)
 
 axios.get(url, {
-  headers: {
-    'Cookie': cookie,  // Add the cookie in the headers
-  }
+    headers: {
+        'Cookie': cookie,  // Add the cookie in the headers
+    }
 }).then(response => p1(response.data))
-.catch((error) => {
-  console.error('Error:', error);
-});
+    .catch((error) => {
+        console.error('Error:', error)
+    })
 
 function p1(rawData) {
-  const left = [];
-  const right = [];
-  const data = rawData.trim().split('\n').map(line => line.split(/\s+/));
-  data.forEach ( lr => {
-    const [l, r] = lr;
-    left.push(parseInt(l));
-    right.push(parseInt(r));
-  });
-  left.sort((a, b) => a-b);
-  right.sort((a, b) => a-b);
+    const left = []
+    const right = []
+    const data = rawData.trim().split('\n').map(line => line.split(/\s+/))
+    data.forEach ( lr => {
+        const [l, r] = lr
+        left.push(parseInt(l))
+        right.push(parseInt(r))
+    })
+    left.sort((a, b) => a-b)
+    right.sort((a, b) => a-b)
   
-  let sum = left.reduce((acc, curr, index) => acc + Math.abs(right[index] - curr), 0);
-  console.log('p1', sum);
-  p2(left, right);
+    let sum = left.reduce((acc, curr, index) => acc + Math.abs(right[index] - curr), 0)
+    console.log('p1', sum)
+    p2(left, right)
 }
 
 function p2(left, right) {
-  let sum = left.reduce((acc, curr) => acc + right.filter(r => r === curr).length * curr, 0);
-  console.log('p2', sum);
+    let sum = left.reduce((acc, curr) => acc + right.filter(r => r === curr).length * curr, 0)
+    console.log('p2', sum)
 }
--- year-2024/day01/script.js
+++ year-2024/day01/script.js
@@ -5,10 +5,10 @@ const preProcessing = input => {
     const left = []
     const right = []
     input.trim().split('\n').map(line => line.split(/\s+/)).forEach(lr => {
-        const [l, r] = lr;
+        const [l, r] = lr
         left.push(parseInt(l))
         right.push(parseInt(r))
-    });
+    })
     left.sort((a, b) => a - b)
     right.sort((a, b) => a - b)
 
@@ -30,7 +30,7 @@ const part1 = input => {
 
 const part2 = input => {
     const {left, right} = preProcessing(input)
-    return left.reduce((acc, curr) => acc + right.filter(r => r === curr).length * curr, 0);
+    return left.reduce((acc, curr) => acc + right.filter(r => r === curr).length * curr, 0)
 }
 
 module.exports = { part1, part2 }
