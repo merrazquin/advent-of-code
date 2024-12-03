a801c9aa8bc3f5595dfab9739b05c3119095e1ab
Marcela Errazquin <merrazquin@gmail.com>
2024-12-02 22:51:50 -0500

    2024.02 light refactoring so var names make more sense

--- year-2024/day02/script.js
+++ year-2024/day02/script.js
@@ -1,21 +1,21 @@
 'use strict'
 
 // Setup
-const preProcessing = input => input.trim().split('\n').map(level => level.split(' ').map(l => parseInt(l)))
+const preProcessing = input => input.trim().split('\n').map(report => report.split(' ').map(l => parseInt(l)))
 
 // Part 1
 // ======
 
-const isLevelSafe = (level, checkDampener = false) => {
-    let joinedLevel = level.join()
-    let ascSortedLevel = level.slice().sort((a, b) => a-b).join()
-    let descSortedLevel = level.slice().sort((a, b) => b-a).join()
+const isReportSafe = (report, checkDampener = false) => {
+    let joinedReport = report.join()
+    let ascSortedReport = report.slice().sort((a, b) => a-b).join()
+    let descSortedReport = report.slice().sort((a, b) => b-a).join()
 
     let safe = false
-    if (joinedLevel === ascSortedLevel || joinedLevel === descSortedLevel) {
-        safe = level.every((val, index) => {
-            if (index < level.length - 1) {
-                const diff = Math.abs(val - level[index + 1]) 
+    if (joinedReport === ascSortedReport || joinedReport === descSortedReport) {
+        safe = report.every((level, index) => {
+            if (index < report.length - 1) {
+                const diff = Math.abs(level - report[index + 1]) 
                 return diff >= 1 && diff <= 3
             }
             return true
@@ -28,24 +28,24 @@ const isLevelSafe = (level, checkDampener = false) => {
         return safe
     }
 
-    for (let i = 0; i < level.length; i++) {
-        let newLevel = level.slice()
-        newLevel.splice(i, 1)
-        if (isLevelSafe(newLevel)) {
+    for (let i = 0; i < report.length; i++) {
+        let newReport = report.slice()
+        newReport.splice(i, 1)
+        if (isReportSafe(newReport)) {
             return true
         }
     }
     return false
 }
 const part1 = input => {
-    return preProcessing(input).filter(level => isLevelSafe(level, false)).length
+    return preProcessing(input).filter(report => isReportSafe(report, false)).length
 }
 
 // Part 2
 // ======
 
 const part2 = input => {
-    return preProcessing(input).filter(level => isLevelSafe(level, true)).length
+    return preProcessing(input).filter(report => isReportSafe(report, true)).length
 }
 
 module.exports = { part1, part2 }
